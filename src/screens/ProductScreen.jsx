6e16726714f9b826685ba6ccff1e35fd52f86b3f
Pablo <pablobt99@gmail.com>
2022-08-06 19:17:05 +0200

    pulling from store

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,26 +1,31 @@
 import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductDetails } from '../actions/productActions'
+
+
 
 function ProductScreen() {
-  const [product, setProduct] = useState([])
+  const dispatch = useDispatch()
+  const productDetails = useSelector(state => state.productDetails)
+  const { loading, error, product} = productDetails
   const {id} = useParams();
   useEffect(() =>{
-    async function fetchProduct(){
-      const { data } = await axios.get(`/api/products/${id}`)
-      setProduct(data)
-    }
-
-    fetchProduct()
-    
+    dispatch(listProductDetails(id))
   }, [])
+
   return (
     <div>
         <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-        <Row>
+        {
+          loading ? <Loader/>
+          : error ? <Message variant="danger">{error}</Message>
+          :
+          <Row>
           <Col md={6}>
             <Image src={product.image} alt={product.name} fluid/>
           </Col>
@@ -74,6 +79,9 @@ function ProductScreen() {
             </Card>
           </Col>
         </Row>
+        }
+        
+        
     </div>
   )
 }
