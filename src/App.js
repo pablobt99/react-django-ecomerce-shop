baa4476a2bbb248afb74c9a825fe73cfcc9e0300
Pablo <pablobt99@gmail.com>
2022-09-03 14:53:05 +0200

    order list

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import OrderListScreen from './screens/OrderListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
 import {Route, Routes} from 'react-router-dom'
 function App() {
@@ -37,6 +38,7 @@ function App() {
             <Route path='/admin/userlist' element={<UserListScreen/>}></Route>
             <Route path='/admin/user/:id/edit' element={<UserEditScreen/>}></Route>
             <Route path='/admin/productlist' element={<ProductListScreen/>}></Route>
+            <Route path='/admin/orderlist' element={<OrderListScreen/>}></Route>
             <Route path='/admin/product/:id/edit' element={<ProductEditScreen/>}></Route>
           </Routes>
         </Container>
